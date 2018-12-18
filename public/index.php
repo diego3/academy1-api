50062d793fc2dfe4d68d7542f1b5cb85a2442d60
diego3 <www.diegosantos.com.br@gmail.com>
2018-12-18 01:37:05 -0200

    ++ docker and composer

--- public/index.php
+++ public/index.php
@@ -1,11 +1,3 @@
 <?php 
-require_once 'Academy1ApiApp.php';
 
-
-try {
-	
-	Academy1ApiApp::resolve();
-} catch (Exception $e) {
-	http_response_code(500);
-	echo 'Application error '.$e->getMessage();
-}
+echo json_encode(['msg' => 'I am alive']);
